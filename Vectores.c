ebe39e4d99e2946e130d62f118b9eca2236af656
Laboratorio <laboratorio@udla-ec.int>
2025-10-24 13:58:32 -0500

    Esta es la segunda parte en la cual se le pone al valor aleatoria a cada numero del vector vector

--- Vectores.c
+++ Vectores.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 #define MAX 100
 int main(int argc, char const *argv[])
 {
     int tamaño;
     int columnas;
     int Macas[columnas];
-
+    int minimo = 1;
+    int maximo = 100;
     printf("Ingrese el numero de columnas:");
     scanf("%d", &tamaño);
 
@@ -19,13 +21,15 @@ int main(int argc, char const *argv[])
 
     for (int i = 0; i < columnas; i++)
     {
-        Macas[columnas] = 0;
+        int numero_aleatorio = minimo + rand() % (maximo - minimo + 1);
+        Macas[columnas+i] = numero_aleatorio;
     }
 
     printf("Impresion de matriz\n");
     for (int i = 0; i < columnas; i++)
     {
-        printf(" %3d ", Macas[columnas]);
+
+        printf(" %3d ", Macas[columnas+i]);
     }
 
     return 0;
